3aa2bde49ce3f4a682ad8c08f17a39330d6b1ea9
glm729 <georgemalone@outlook.com.au>
2021-01-17 08:41:13 +0800

    Minor changes to ruby/printlength.rb

--- ruby/printlength.rb
+++ ruby/printlength.rb
@@ -1,29 +1,26 @@
 #!/usr/bin/ruby
 
-# ==============================================================================
-# Prints a body of text at the specified length. Written when writing an example
-# for a literature review that involved printing a large piece of unstructured
-# text (a veterinarian's examination summary notes), which runs off the page of
-# an R Markdown document. Thought I'd try Ruby for a change.
-# ==============================================================================
+# Prints a body of text at the specified length.  Written when writing an
+# example for a literature review that involved printing a large piece of
+# unstructured text (a veterinarian's examination summary notes), which runs
+# off the page of an R Markdown document.  Thought I'd try Ruby for a change.
+
 # - Default length is 80.
 # - Haven't yet incorporated checks for correct datatypes.
-# - I do not know anything about Ruby! Constructive comments are most welcome.
-# ==============================================================================
+# - I know a bit more about Ruby but I'm still pretty much a novice, so
+#   comments and tips are most welcome.
 
-def printlen(text, len = 80)
+def printlength(text, len = 80)
   t = text.to_s.split
-  lines = [""]
+  lines = ['']
   i = 0
   t.each do |word|
     if word.length + lines[i].length > len.to_i
       i += 1
-      lines[i] = ""
+      lines[i] = ''
     end
     lines[i].concat("#{word} ")
   end
-  (0..lines.length).each do |j|
-    lines[j].strip! if lines[j].class != NilClass
-  end
-  print(lines.join("\n").concat("\n"))
+  lines.map{|l| l.strip!}
+  return "#{lines.join("\n")}\n"
 end
