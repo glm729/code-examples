a8d95eff3994c23075f8c69ceb6b08c751e40122
glm729 <georgemalone@outlook.com.au>
2021-07-24 15:12:11 +0800

    Converted update_wclass.py to use Message.py
    - All messages use the Message class
    - Used Message(...).text in the is_upgrade method
    - No need for the _msg method any more
    - Basic testing appears to be working, but as before there is probably
      something that hasn't been checked

--- python/update_wclass.py
+++ python/update_wclass.py
@@ -7,6 +7,7 @@
 
 import re
 import subprocess as sp
+from Message import Message
 
 
 # Class definition
@@ -23,7 +24,7 @@ class Updater:
         self.rex_get_n = re.compile("^(?P<n>\d+) packages? can be upgraded")
 
     def apt_update(self):
-        self._msg("info", "Updating package lists...")
+        Message("Updating package lists...", form="info").print()
         output = sp.run(self.cmd_apt_update, capture_output=True).stdout
         text = ''.join(chr(c) for c in output).rstrip().split("\n")
         self.result_update = text
@@ -36,7 +37,8 @@ class Updater:
         result_list = list(map(func, text))
         pretty_list = f'\n{9 * " "}'.join(result_list)
         self.result_list = result_list
-        self._msg("ok", f'Package{self.s} to upgrade:\n{9 * " "}{pretty_list}')
+        _msg = f'Package{self.s} to upgrade:\n{9 * " "}{pretty_list}'
+        Message(_msg, form="ok").print()
         return result_list
 
     def _get_n_from_last(self):
@@ -45,7 +47,8 @@ class Updater:
             return 0
         if (match := self.rex_get_n.match(last)) is not None:
             return int(match["n"])
-        print(f'\033[7;31m ERROR \033[0m Message not recognised:\n{last}')
+        _msg = f'\033[7;31m ERROR \033[0m Message not recognised:\n{last}'
+        Message(_msg, form="exit").print()
         raise RuntimeError("apt update message not recognised")
 
     def check_update(self):
@@ -53,37 +56,25 @@ class Updater:
         self.s = '' if n == 1 else "s"
         self.num_upgrade = n
         if n > 0:
-            self._msg(
-                "info",
-                f'{n} package{self.s} to upgrade, fetching list...')
+            _msg = f'{n} package{self.s} to upgrade, fetching list...'
+            Message(_msg, form="info").print()
             self.apt_list()
         else:
-            self._msg("ok", "Nothing to upgrade")
+            Message("Nothing to upgrade", form="ok").print()
         return n
 
     def apt_upgrade(self):
         if self.is_upgrade():
-            self._msg("ok", f'Upgrading package{self.s}')
+            Message(f'Upgrading package{self.s}', form="ok").print()
             sp.run(self.cmd_apt_upgrade)
 
     def is_upgrade(self):
-        msg = f'\033[7;35m  IP  \033[0m Upgrade package{self.s}? [y/n]: '
+        msg = Message(f'Upgrade package{self.s}? [y/n]: ', form="input").text
         upgrade = input(msg).lower()
         while not upgrade in ["y", "n"]:
-            self._msg("warn", "Please enter only y or n")
+            Message("Please enter only y or n", form="warn").print()
             upgrade = "y" if is_upgrade() else "n"
         return upgrade == "y"
-
-    def _msg(self, ty, tx):
-        fmt = {
-            "exit": "\033[7;31m EXIT \033[0m ",
-            "ok": "\033[7;32m  OK  \033[0m ",
-            "warn": "\033[7;33m WARN \033[0m ",
-            "info": "\033[7;34m INFO \033[0m ",
-            "input": "\033[7;35m  IP  \033[0m ",
-            "data": "\033[7;36m DATA \033[0m "
-        }
-        print(f'{fmt[ty]}{tx}')
 
 
 # Operations
@@ -100,7 +91,7 @@ runner.check_update()
 # If at least one package to upgrade, run the upgrade
 if runner.num_upgrade > 0:
     runner.apt_upgrade()
-    runner._msg("ok", "End of operations")
+    Message("End of operations", form="ok").print()
 
 # All done
 exit(0)
